decf4e156831e1dd36e5b693a0cfa2236779c5a4
dolsotbob <1hyoon3@naver.com>
2025-02-21 14:51:56 +0900

    modified

--- src/script.js
+++ src/script.js
@@ -1,9 +1,34 @@
-// TODO : DOM으로부터 필요한 엘리먼트를 불러오세요.
+let elInputUsername = document.querySelector('#username');
+let elFailureMessage = document.querySelector('.failure-message');
+let elSuccessMessage = document.querySelector('.success-message');
+
+elInputUsername.onkeyup = function () {
+  if (isMoreThan4Length(elInputUsername.value)) {
+    elSuccessMessage.classList.remove('hide');
+    elFailureMessage.classList.add('hide');
+  } else {
+    elSuccessMessage.classList.add('hide');
+    elFailureMessage.classList.remove('hide');
+  }
+};
 
 function isMoreThan4Length(value) {
-  // TODO : 이 함수를 완성하세요.
+  return value.length >= 4;
 }
 
+
+let elInputPassword = document.querySelector('#password');
+let elInputPasswordRetype = document.querySelector('#password-retype');
+let elMismatchMessage = document.querySelector('.mismatch-message');
+
 function isMatch(password1, password2) {
-  // TODO : 이 함수를 완성하세요.
+  return password1 === password2;
 }
+
+elInputPasswordRetype.onkeyup = function () {
+  if (isMatch(elInputPassword.value, elInputPasswordRetype.value)) {
+    elMismatchMessage.classList.add('hide');
+  } else {
+    elMismatchMessage.classList.remove('hide');
+  }
+};
